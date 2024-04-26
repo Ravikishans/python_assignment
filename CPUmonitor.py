0826538b94b4695d323d4e5781cbd00bf0924920
Ravikishan <ravikishan1996@gmail.com>
2024-04-26 18:37:48 +0530

    psutilusages

--- CPUmonitor.py
+++ CPUmonitor.py
@@ -6,10 +6,20 @@ def monitor_cpu(threshold):
     try:
 
         while True:
-            cpu_usage = psutil.cpu_percent(interval=1)
-            if cpu_usage > threshold:
-                print(f"Alert! CPU usage exceeds threshold: {cpu_usage}%")
-            time.sleep(1)
+            cpu_usage = psutil.cpu_percent(interval=5)
+            mem_usage= psutil.virtual_memory().percent
+            cpustat=psutil.cpu_stats()
+            cpufreq=psutil.cpu_freq()
+            # diskusages=psutil.disk_usage("C:\Users\Ravik").percent
+            
+            if cpu_usage > threshold or mem_usage >80:
+            
+                print(f"Alert! CPU usage exceeds threshold: {cpu_usage}%,{mem_usage}")
+                print('\n',cpustat)
+                print('\n',cpufreq)
+                # print(diskusages)
+
+            time.sleep(5)
     except KeyboardInterrupt:  
 # by pressing ctrl+c
         print("Monitoring stopped.")
